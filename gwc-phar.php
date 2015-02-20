28504f30f194ec95e25aa67183eb4cd2f357f356
masdeft <masdeft@gmail.com>
2015-02-20 15:06:43 +0200

    .gitignore is added

--- gwc-phar.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-$phar = new Phar("./gwc.phar", FilesystemIterator::CURRENT_AS_FILEINFO |FilesystemIterator::KEY_AS_FILENAME, "gwc.phar");
-$phar->buildFromDirectory('./');
-$phar->setStub($phar->createDefaultStub("public/make.php"));
-
- 
